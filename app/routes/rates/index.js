52ed5cc12131a6219ade5888f93dafadc1f9feaf
ke-pan <ke_pan@yeah.net>
2016-06-07 17:49:53 +0800

    feat: rates index

--- app/routes/rates/index.js
+++ app/routes/rates/index.js
@@ -2,16 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model() {
-    // let restaurant = this.modelFor("restaurants/show");
     return this.modelFor("restaurants/show").get('rates');
-    // return this.store.findAll('rate', {
-    //   "where": {
-    //     "restaurant": {
-    //       "__type": "Pointer",
-    //       "className": "Restaurant",
-    //       "objectId": restaurant.id
-    //     }
-    //   }
-    // });
   }
 });
