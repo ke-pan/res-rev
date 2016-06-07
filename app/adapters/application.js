bdebd14893782b5845b44ea2f5007c839074b7fb
ke-pan <ke_pan@yeah.net>
2016-06-07 08:12:07 +0800

    refactor: change leancloud to rails server

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,15 +1,5 @@
-import Ember from 'ember';
-import RESTAdapter from 'ember-data/adapters/rest';
+import JSONAPIAdapter from 'ember-data/adapters/json-api';
 
-export default RESTAdapter.extend({
-  host: "https://api.leancloud.cn",
-  namespace: "1.1/classes",
-  headers: {
-    "X-LC-Id": "4SOxXkuFjFAR8E6KIkRRx2ss-gzGzoHsz",
-    "X-LC-Key": "0WGOggyaYusMqxvq9d5J2I0K"
-  },
-  pathForType(modelName) {
-    var camelized = Ember.String.camelize(modelName);
-    return Ember.String.capitalize(camelized);
-  }
+export default JSONAPIAdapter.extend({
+  namespace: "api"
 });
--- app/serializers/application.js
+++
@@ -1,8 +0,0 @@
-import DS from 'ember-data';
-
-export default DS.JSONSerializer.extend({
-  primaryKey: 'objectId',
-  normalizeFindAllResponse(store, model, payload, id, requestType) {
-    return this._super(store, model, payload.results, id, requestType);
-  }
-});
