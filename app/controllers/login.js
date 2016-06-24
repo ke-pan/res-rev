e84daaaa761756b2f6b228b5778bff1bee413ddb
ke-pan <ke_pan@yeah.net>
2016-06-24 17:39:36 +0800

    feat: login server token endpoint

--- app/controllers/login.js
+++ app/controllers/login.js
@@ -5,9 +5,10 @@ export default Ember.Controller.extend({
   actions: {
     login() {
       let { name, password } = this.getProperties('name', 'password');
-      this.get('session').authenticate('authenticator:oauth2', name, password).catch((reason) => {
-        this.set('errorMessage', reason.error || reason);
-      });
+      this.get('session').authenticate('authenticator:oauth2', name, password)
+        .catch((reason) => {
+          this.set('errorMessage', reason.error || reason);
+        });
     }
   }
 });
