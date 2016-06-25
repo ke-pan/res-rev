c053017b9e833545febd2b25794295c2a3bb471e
ke-pan <ke_pan@yeah.net>
2016-06-25 09:17:55 +0800

    feat: rates style

--- app/models/rate.js
+++ app/models/rate.js
@@ -3,5 +3,6 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   comment: DS.attr('string'),
   score: DS.attr('number'),
-  restaurant: DS.belongsTo('restaurant')
+  restaurant: DS.belongsTo('restaurant'),
+  user: DS.belongsTo('user')
 });
