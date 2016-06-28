62e271b2047701ccc2046b90b27a485e387aaf5d
ke-pan <ke_pan@yeah.net>
2016-06-28 21:58:05 +0800

    feat: star-selection component

--- app/components/star-option.js
+++ app/components/star-option.js
@@ -2,9 +2,19 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   tagName: 'li',
+  classNames: ['star'],
   classNameBindings: ['starOrder'],
   starOrder: Ember.computed('star', function() {
     let star = this.get('star');
-    return 'star' + star
-  })
+    return 'star' + star;
+  }),
+  click() {
+    this.get('clickStar')(this.get('starOrder'));
+  },
+  mouseEnter() {
+    this.get('mouseInStar')(this.get('starOrder'));
+  },
+  mouseLeave() {
+    this.get('mouseOutStar')();
+  }
 });
--- app/components/star-selection.js
+++ app/components/star-selection.js
@@ -2,5 +2,21 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   tagName: 'ul',
-  classNames: ['star-selection']
+  classNames: ['star-selection'],
+  lastStar: 'star1',
+  actions: {
+    commitActiveStar(star) {
+      this.$('.star').removeClass('active-star');
+      this.$('.' + star).addClass('active-star');
+      this.set('lastStar', star);
+    },
+    changeActiveStar(star) {
+      this.$('.star').removeClass('active-star');
+      this.$('.' + star).addClass('active-star');
+    },
+    rollbackActiveStar() {
+      this.$('.star').removeClass('active-star');
+      this.$('.' + this.get('lastStar')).addClass('active-star');
+    }
+  }
 });
