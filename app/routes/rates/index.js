43c449207fa39e2e46988fbb1017b6f8346c1c5c
ke-pan <ke_pan@yeah.net>
2016-06-29 08:40:45 +0800

    feat: stay current page after login

--- app/routes/rates/index.js
+++ app/routes/rates/index.js
@@ -1,6 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  session: Ember.inject.service('session'),
+  beforeModel(transition) {
+    if (!this.get('session.isAuthenticated')) {
+      this.set('session.attemptedTransition', transition);
+    }
+  },
   model() {
     return this.modelFor("restaurants/show").get('rates');
   }
