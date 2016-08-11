58e6beb3a059d82cef8b5f810193e253eff94e0e
ke-pan <ke_pan@yeah.net>
2016-08-11 08:11:29 +0800

    feat: review stars

--- mirage/factories/review.js
+++ mirage/factories/review.js
@@ -3,6 +3,6 @@ import { Factory, faker } from 'ember-cli-mirage';
 export default Factory.extend({
   comment() { return faker.lorem.paragraph(); },
   reviewerName() { return faker.name.firstName(); },
-  score() { return faker.random.number(5); },
+  score() { return faker.random.number({min:2, max:5}); },
   reviewDate() { return faker.date.recent(); }
 });
