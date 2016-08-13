f44064d5921820ec2b754e4d34cf362a6c8f8686
ke-pan <ke_pan@yeah.net>
2016-08-13 17:56:48 +0800

    web accessibility

--- app/components/review-stars.js
+++ app/components/review-stars.js
@@ -1,26 +1,27 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  classNames: ["review-stars"],
   classNameBindings: ["reviewStar"],
   reviewStar: Ember.computed("score", function() {
     let score = this.get("score");
     let className;
     if (score > 4.8) {
-      className = "review-5";
+      className = "star-5";
     } else if (score > 4.3) {
-      className = "review-4-5";
+      className = "star-4-5";
     } else if (score > 3.8) {
-      className = "review-4";
+      className = "star-4";
     } else if (score > 3.3) {
-      className = "review-3-5";
+      className = "star-3-5";
     } else if (score > 2.8) {
-      className = "review-3";
+      className = "star-3";
     } else if (score > 1.8) {
-      className = "review-2";
+      className = "star-2";
     } else if (score > 0.8) {
-      className = "review-1";
+      className = "star-1";
     } else {
-      className = "review-0";
+      className = "star-0";
     }
     return className;
   })
